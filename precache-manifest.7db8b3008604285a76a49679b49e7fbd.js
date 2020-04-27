0b1c3880c1fe77dbd01bb94198ca291b795c78ad
Ziomioslaw <ziomioslaw@wp.pl>
2020-04-27 22:39:25 +0200

    Uploading pwa-vue-calculator version 1.0

---
+++ precache-manifest.7db8b3008604285a76a49679b49e7fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f905b76894759685d34",
+    "url": "/css/app.6df2d4a7.css"
+  },
+  {
+    "revision": "a2557c9ea9e11e4868cf4a9e3982395a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f905b76894759685d34",
+    "url": "/js/app.6a52de41.js"
+  },
+  {
+    "revision": "82fa2e23eec7e56d8dc2",
+    "url": "/js/chunk-vendors.1def14f3.js"
+  },
+  {
+    "revision": "7840d8974799a47f8d7badfe4fafe4e7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
